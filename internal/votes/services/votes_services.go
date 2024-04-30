69435606a91e2fbd55db4778dde6a43d59cab5bd
stephan <stephan102002@gmail.com>
2024-04-29 21:56:44 -0500

    publishing a msg in a topic in pub/sub

--- internal/votes/services/votes_services.go
+++ internal/votes/services/votes_services.go
@@ -1,8 +1,16 @@
 package services
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"cloud.google.com/go/pubsub"
 	entity "example.com/module/internal/votes/entity"
 	repository "example.com/module/internal/votes/repository/mongo"
+	"github.com/joho/godotenv"
 )
 
 type voteService struct {
@@ -25,5 +33,44 @@ func (vs *voteService) SaveVote(entityVote *entity.Votes) error {
 		return err
 	}
 
+	msg := fmt.Sprintf(`{"userID": "%s", "roomID": "%s", "userStory": "%s", "value": "%s"}`, entityVote.UserId, entityVote.RoomId, entityVote.UserStory, entityVote.Value)
+
+	err = publishThatScales(os.Stdout, msg)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+var projectID string
+
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+	projectID = os.Getenv("PROJECT_ID")
+}
+
+func publishThatScales(w io.Writer, msg string) error {
+	topicID := "votes"
+	// msg := "Hello World"
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub: NewClient: %w", err)
+	}
+	defer client.Close()
+
+	t := client.Topic(topicID)
+	result := t.Publish(ctx, &pubsub.Message{
+		Data: []byte(msg),
+	})
+
+	id, err := result.Get(ctx)
+	if err != nil {
+		return fmt.Errorf("pubsub: result.Get: %w", err)
+	}
+	fmt.Fprintf(w, "Published a message; msg ID: %v\n", id)
 	return nil
 }
